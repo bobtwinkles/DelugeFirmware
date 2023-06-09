0a2b842c10cfaa4df4e1a31a70537dd5a1640262
bobtwinkles <srkoser+github@gmail.com>
2023-06-08 22:35:23 -0700

    chore: fix -Wwrite-strings failures

--- src/ModControllableAudio.cpp
+++ src/ModControllableAudio.cpp
@@ -1461,7 +1461,7 @@ void ModControllableAudio::endStutter(ParamManagerForTimeline* paramManager) {
 void ModControllableAudio::switchDelayPingPong() {
 	delay.pingPong = !delay.pingPong;
 
-	char* displayText;
+	char const* displayText;
 	switch (delay.pingPong) {
 	case 0:
 		displayText = "Normal delay";
